ba52dbf7b2a76f45da28db49981cc7a7e58c007b
alexsylvian <lazeratus1@gmail.com>
2023-11-16 23:31:09 -0500

    28 side routing tests passed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,27 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
+    <nav>
+      <NavLink
+        to="/"
+        className="navbar"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/actors"
+        className="navbar"
+      >
+        Actors
+      </NavLink>
+      <NavLink
+        to="/directors"
+        className="navbar"
+      >
+        Directors
+      </NavLink>
     </nav>
-    );
+  );
 };
 
 export default NavBar;
